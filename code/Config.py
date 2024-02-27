e2a027431b198aa5aab0cca531a14d2f5fb40144
Adam Micolich <adam.micolich@gmail.com>
2024-02-27 15:32:12 +1100

    End of day 27FEB24

--- code/Config.py
+++ code/Config.py
@@ -8,11 +8,15 @@ This informs various parts of the software about aspects of your bench setup
 
 # Information about which Raspberry Pi you are using (MeasureOne, MeasureTwo, etc)
 # Details for the various Pis are in Pi_control.py
-PiBox = 'MeasureThree'
+PiBox = 'MeasureOne'
 
 # Information about which Truth Table to use (Devices, Rows, etc)
 # Details are in Pi-control.py but Test is for hardware test (devices), Run is for measurements with two pre-amps (rows) on Gen 3a/4 MuxBoards
 MuxMode = 'Run'
+
+# Information about which Multiplexer Box you are using
+# Gen3a is for blue PCBs without the power-circuit relay, Gen4 is for red PCBs with the power-circuit relay.
+MuxGen = 'Gen4'
 
 # Information about which NIDAQ ports you are using
 Source = 'Dev1/ao0'
