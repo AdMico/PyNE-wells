ae4e3dc03a21b28c460429a7c9465ba904b56307
Adam Micolich <adam.micolich@gmail.com>
2024-04-15 15:52:57 +1000

    End of Day 15APR24

--- code/Config.py
+++ code/Config.py
@@ -8,7 +8,7 @@ This informs various parts of the software about aspects of your bench setup
 
 # Information about which Raspberry Pi you are using (MeasureOne, MeasureTwo, etc)
 # Details for the various Pis are in Pi_control.py
-PiBox = 'MeasureOne'
+PiBox = 'MeasureTwo'
 
 # Information about which Truth Table to use (Devices, Rows, etc)
 # Details are in Pi-control.py but Test is for hardware test (devices), Run is for measurements with two pre-amps (rows) on Gen 3a/4 MuxBoards
